2a70a9f7fc5fcd5ec3edaeddd170f90d12c0f48e
Kamesh Ganesan <kameshg@kainos.com>
2019-06-11 13:59:47 +0100

    IMTA-5223 schema changes for control confirmation page

--- imports-frontend-entities/src/entities/feedback_information.js
+++ imports-frontend-entities/src/entities/feedback_information.js
@@ -10,6 +10,8 @@ module.exports = class FeedBackInformation {
     this.authorityType = obj.authorityType
     this.consignmentArrival = obj.consignmentArrival
     this.consignmentConformity = obj.consignmentConformity
+    this.consignmentNoArrivalReason = obj.consignmentNoArrivalReason
+    this.destructionDate = obj.destructionDate
 
     return Object.seal(new Proxy(this, handler))
   }
