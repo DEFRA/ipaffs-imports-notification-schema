fa159c4c13e9794920473f075e90215170b35d24
Mateusz Kalinowski <M.Kalinowski@kainos.com>
2019-10-30 11:12:38 +0100

    IMTA-5995 Species identifiers fixes

--- notification-schema-java/src/main/java/uk/gov/defra/tracesx/notificationschema/representation/Identifier.java
+++ notification-schema-java/src/main/java/uk/gov/defra/tracesx/notificationschema/representation/Identifier.java
@@ -2,12 +2,13 @@ package uk.gov.defra.tracesx.notificationschema.representation;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import java.util.Map;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Map;
 
 @Builder
 @Data
