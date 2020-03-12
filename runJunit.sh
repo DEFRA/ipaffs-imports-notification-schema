07bf6dbe3065dae6b72c478f51e0576cb8b049cf
Marcus Bond <marcusb@kainos.com@Masterplan.local>
2020-03-12 11:02:42 +0000

    IMTA-6780: Set the sonar properties to report code coverage

--- runJunit.sh
+++ runJunit.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-mvn test -f pom.xml
+export JAVA_HOME=$JAVA_HOME_11
+mvn clean test -f imports-frontend-entities/pom-sonar.xml --settings notification-schema-core/vs-maven.xml -Dmaven.repo.local=${WORKSPACE}/.m2
+mvn clean test verify -f pom.xml --settings notification-schema-core/vs-maven.xml -Dmaven.repo.local=${WORKSPACE}/.m2
+sed -i.bak 's/path="test/path="imports-frontend-entities\/test/g' imports-frontend-entities/coverage/unit.xml
