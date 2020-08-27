c36d3965593d4b1a99735016c56fe1e3cdf77862
Reece Bennett <reeceb@kainos.com>
2020-08-27 13:56:16 +0100

    IMTA-7970: Add contact details object to schema

---
+++ imports-frontend-entities/src/entities/contact_details.js
@@ -0,0 +1,16 @@
+const handler = require('./base/handler')
+
+module.exports = class ContactDetails {
+  constructor(obj) {
+    if (!obj) {
+      obj = {}
+    }
+
+    this.name = obj.name
+    this.telephone = obj.telephone
+    this.email = obj.email
+    this.agent = obj.agent
+
+    return Object.seal(new Proxy(this, handler))
+  }
+}
