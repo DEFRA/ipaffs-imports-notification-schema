18e018e91112c00937815f32f9a13d0ccc78943b
Sean Treanor <seant@kainos.com>
2020-09-07 17:59:56 +0100

    IMTA-8177: Imprioving code coverage on imports-notification-schema

---
+++ notification-schema-java/src/test/java/uk/gov/defra/tracesx/notificationschema/representation/serialisation/IsoOffsetDateTimeDeserializerTest.java
@@ -0,0 +1,39 @@
+package uk.gov.defra.tracesx.notificationschema.representation.serialisation;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import com.fasterxml.jackson.core.JsonParser;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@RunWith(MockitoJUnitRunner.class)
+public class IsoOffsetDateTimeDeserializerTest {
+
+  private IsoOffsetDateTimeDeserializer isoOffsetDateTimeDeserializer = new IsoOffsetDateTimeDeserializer();
+
+  @Mock
+  private JsonParser jsonParser;
+
+  @Test
+  public void givenAValidDateTimeString_whenDeserializerCalled_expectLocalDate() throws IOException {
+    // given
+    when(jsonParser.getText()).thenReturn("2020-08-04T15:45:37.029947Z");
+
+    // when
+    LocalDateTime result = isoOffsetDateTimeDeserializer.deserialize(jsonParser, null);
+
+    // then
+    LocalDate expectedDate = LocalDate.of(2020, 8, 4);
+    LocalTime expectedTime = LocalTime.of(15, 45, 37, 29947000);
+    assertThat(result.toLocalDate()).isEqualTo(expectedDate);
+    assertThat(result.toLocalTime()).isEqualTo(expectedTime);
+  }
+}
