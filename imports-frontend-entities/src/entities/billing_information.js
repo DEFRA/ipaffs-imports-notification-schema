ef32b98fe2648586b3d6282cb09fdc85e1b6a723
ethan.weatherup <ethan.weatherup@defra.onmicrosoft.com>
2024-02-01 12:00:06 +0000

    IMTA-16576: Add CUC fields to schema

---
+++ imports-frontend-entities/src/entities/billing_information.js
@@ -0,0 +1,14 @@
+const handler = require('./base/handler')
+const PostalAddress = require('./postal_address')
+
+module.exports = class BillingInformation {
+
+  constructor(obj = {}) {
+
+    this.emailAddress = obj.emailAddress
+    this.phoneNumber = obj.phoneNumber
+    this.postalAddress = obj.postalAddress ? new PostalAddress(obj.postalAddress) : undefined
+
+    return Object.seal(new Proxy(this, handler))
+  }
+}
