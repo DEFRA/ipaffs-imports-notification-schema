1decd67d71982979dd6ea47d139333218956961f
josh.craig <josh.craig@defra.onmicrosoft.com>
2024-03-22 11:36:36 +0000

    reverting previous commit

--- imports-frontend-entities/src/entities/billing_information.js
+++ imports-frontend-entities/src/entities/billing_information.js
@@ -5,7 +5,6 @@ module.exports = class BillingInformation {
 
   constructor (obj = {}) {
 
-    this.isConfirmed = obj.isConfirmed
     this.emailAddress = obj.emailAddress
     this.phoneNumber = obj.phoneNumber
     this.contactName = obj.contactName
--- notification-schema-java/src/main/java/uk/gov/defra/tracesx/notificationschema/representation/BillingInformation.java
+++ notification-schema-java/src/main/java/uk/gov/defra/tracesx/notificationschema/representation/BillingInformation.java
@@ -15,9 +15,6 @@ import lombok.extern.jackson.Jacksonized;
 public class BillingInformation {
 
   @NotBlank
-  private Boolean isConfirmed;
-
-  @NotBlank
   private String emailAddress;
 
   @NotBlank
