1ccf7009caaa1ae581d68f9fd559e3a1253dd998
ian.griffiths <ian.griffiths@defra.onmicrosoft.com>
2025-03-07 13:58:04 +0000

    IMTA-19531 - Remove all other exisitng eu conditions, this flow is just for users

--- notification-schema-java/src/test/java/uk/gov/defra/tracesx/notificationschema/validation/annotations/ChedaDocumentCheckResultValidatorTest.java
+++ notification-schema-java/src/test/java/uk/gov/defra/tracesx/notificationschema/validation/annotations/ChedaDocumentCheckResultValidatorTest.java
@@ -1,12 +1,7 @@
 package uk.gov.defra.tracesx.notificationschema.validation.annotations;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static uk.gov.defra.tracesx.notificationschema.representation.enumeration.Result.DEROGATION;
-import static uk.gov.defra.tracesx.notificationschema.representation.enumeration.Result.NOT_DONE;
-import static uk.gov.defra.tracesx.notificationschema.representation.enumeration.Result.NOT_SATISFACTORY;
 import static uk.gov.defra.tracesx.notificationschema.representation.enumeration.Result.NOT_SET;
-import static uk.gov.defra.tracesx.notificationschema.representation.enumeration.Result.SATISFACTORY;
-import static uk.gov.defra.tracesx.notificationschema.representation.enumeration.Result.SATISFACTORY_FOLLOWING_OFFICIAL_INTERVENTION;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -28,145 +23,13 @@ class ChedaDocumentCheckResultValidatorTest {
   }
 
   @Test
-  void isValid_returnsFalse_whenDocumentCheckSatisfactoryEuStandardAndNationalRequirementNull() {
-    assertThat(validator.isValid(check, null)).isFalse();
-  }
-
-  @Test
   void isValid_returnsTrue_whenConsignmentCheckIsNull() {
     assertThat(validator.isValid(null, null)).isTrue();
-  }
-
-  @Test
-  void isValid_returnsTrue_whenDocumentCheckSatisfactoryEuStandardNotSetAndNationalRequirementNotSet() {
-    check.setDocumentCheckResult(SATISFACTORY);
-    check.setEuStandard(NOT_SET);
-    check.setNationalRequirements(NOT_SET);
-
-    assertThat(validator.isValid(check, null)).isTrue();
-  }
-
-  @Test
-  void isValid_returnsTrue_whenDocumentCheckNotSatisfactoryEuStandardNotSetAndNationalRequirementNotSet() {
-    check.setDocumentCheckResult(NOT_SATISFACTORY);
-    check.setEuStandard(NOT_SET);
-    check.setNationalRequirements(NOT_SET);
-
-    assertThat(validator.isValid(check, null)).isTrue();
-  }
-
-  @Test
-  void isValid_returnsFalse_whenDocumentCheckEuStandardAndNationalRequirementNotSatisfactory() {
-    check.setDocumentCheckResult(NOT_SATISFACTORY);
-    check.setEuStandard(NOT_SATISFACTORY);
-    check.setNationalRequirements(NOT_SATISFACTORY);
-
-    assertThat(validator.isValid(check, null)).isFalse();
-  }
-
-  @Test
-  void isValid_returnsFalse_whenDocumentCheckEuStandardAndNationalRequirementSatisfactory() {
-    check.setDocumentCheckResult(SATISFACTORY);
-    check.setEuStandard(SATISFACTORY);
-    check.setNationalRequirements(SATISFACTORY);
-
-    assertThat(validator.isValid(check, null)).isFalse();
-  }
-
-  @Test
-  void isValid_returnsFalse_whenDocumentCheckOfficialInterventionEuStandardNotDoneAndNationalRequirementDerogation() {
-    check.setDocumentCheckResult(SATISFACTORY_FOLLOWING_OFFICIAL_INTERVENTION);
-    check.setEuStandard(NOT_DONE);
-    check.setNationalRequirements(DEROGATION);
-
-    assertThat(validator.isValid(check, null)).isFalse();
-  }
-
-  @Test
-  void isValid_returnsTrue_whenDocumentCheckNotSetEuStandardSatisfactoryAndNationalRequirementSatisfactory() {
-    check.setDocumentCheckResult(NOT_SET);
-    check.setEuStandard(SATISFACTORY);
-    check.setNationalRequirements(SATISFACTORY);
-
-    assertThat(validator.isValid(check, null)).isTrue();
-  }
-
-  @Test
-  void isValid_returnsTrue_whenDocumentCheckNotSetEuStandardNotSatisfactoryAndNationalRequirementNotSatisfactory() {
-    check.setDocumentCheckResult(NOT_SET);
-    check.setEuStandard(NOT_SATISFACTORY);
-    check.setNationalRequirements(NOT_SATISFACTORY);
-
-    assertThat(validator.isValid(check, null)).isTrue();
-  }
-
-  @Test
-  void isValid_returnsTrue_whenDocumentCheckNotSetEuStandardNotSatisfactoryAndNationalRequirementSatisfactory() {
-    check.setDocumentCheckResult(NOT_SET);
-    check.setEuStandard(NOT_SATISFACTORY);
-    check.setNationalRequirements(SATISFACTORY);
-
-    assertThat(validator.isValid(check, null)).isTrue();
-  }
-
-  @Test
-  void isValid_returnsTrue_whenDocumentCheckNotSetEuStandardSatisfactoryAndNationalRequirementNotSatisfactory() {
-    check.setDocumentCheckResult(NOT_SET);
-    check.setEuStandard(SATISFACTORY);
-    check.setNationalRequirements(NOT_SATISFACTORY);
-
-    assertThat(validator.isValid(check, null)).isTrue();
-  }
-
-  @Test
-  void isValid_returnsFalse_whenDocumentCheckEuStandardAndNationalRequirementNotSet() {
-    check.setDocumentCheckResult(NOT_SET);
-    check.setEuStandard(NOT_SET);
-    check.setNationalRequirements(NOT_SET);
-
-    assertThat(validator.isValid(check, null)).isFalse();
-  }
-
-  @Test
-  void isValid_returnsTrue_whenDocumentCheckEuStandardNotSetNationalRequirementSet() {
-    check.setDocumentCheckResult(NOT_SET);
-    check.setEuStandard(NOT_SET);
-    check.setNationalRequirements(SATISFACTORY);
-
-    assertThat(validator.isValid(check, null)).isTrue();
-  }
-
-  @Test
-  void isValid_returnsTrue_whenDocumentCheckNotSetEuStandardNotSatisfactoryNationalRequirementSatisFactory() {
-    check.setDocumentCheckResult(NOT_SET);
-    check.setEuStandard(NOT_SATISFACTORY);
-    check.setNationalRequirements(SATISFACTORY);
-
-    assertThat(validator.isValid(check, null)).isTrue();
-  }
-
-  @Test
-  void isValid_returnsTrue_whenDocumentCheckEuStandardNullAndNationalRequirementNotSet() {
-    check.setDocumentCheckResult(NOT_SET);
-    check.setNationalRequirements(NOT_SET);
-
-    assertThat(validator.isValid(check, null)).isTrue();
-  }
-
-  @Test
-  void isValid_returnsTrue_whenDocumentCheckSatisfactoryEuStandardAndNationalRequirementNull() {
-    check.setDocumentCheckResult(SATISFACTORY);
-    check.setEuStandard(NOT_SET);
-
-    assertThat(validator.isValid(check, null)).isFalse();
   }
 
   @ParameterizedTest
   @EnumSource(Result.class)
   void isValid_returnsTrue_ForDocumentCheckResultsThatAreNotNotSet(Result result) {
-    check.setEuStandard(NOT_SET);
-    check.setNationalRequirements(NOT_SET);
-
     check.setDocumentCheckResult(result);
 
     if (result != NOT_SET) {
