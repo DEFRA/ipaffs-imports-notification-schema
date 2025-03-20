f45ce0208f551b54b0c0861528c056cf94ad85b5
andrew.harrison <andrew.harrison@Defra.onmicrosoft.com>
2025-03-20 17:20:29 +0000

    IMTA-19836 Added additional validation under CHEDA when horses are being transported under temporary admission

---
+++ notification-schema-java/src/main/java/uk/gov/defra/tracesx/notificationschema/validation/annotations/NotNullPurposeExitDateValidator.java
@@ -0,0 +1,29 @@
+package uk.gov.defra.tracesx.notificationschema.validation.annotations;
+
+import static uk.gov.defra.tracesx.notificationschema.representation.enumeration.ForImportOrAdmissionEnum.TEMPORARY_ADMISSION_HORSES;
+import static uk.gov.defra.tracesx.notificationschema.representation.enumeration.PurposeGroupEnum.TRANSIT_TO_3RD_COUNTRY;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import uk.gov.defra.tracesx.notificationschema.representation.PartOne;
+import uk.gov.defra.tracesx.notificationschema.representation.Purpose;
+
+public class NotNullPurposeExitDateValidator implements
+    ConstraintValidator<NotNullPurposeExitBip, PartOne> {
+
+  @Override
+  public boolean isValid(PartOne partOne, ConstraintValidatorContext constraintValidatorContext) {
+
+    if (partOne == null || partOne.getPurpose() == null) {
+      return true;
+    }
+
+    Purpose purpose = partOne.getPurpose();
+
+    if (TEMPORARY_ADMISSION_HORSES.equals(purpose.getForImportOrAdmission())) {
+      return purpose.getExitDate() != null;
+    }
+
+    return true;
+  }
+}
