1ba33e8e9e197901364df95d7e6cc3b13defe0e3
will.crossland <will.crossland@defra.onmicrosoft.com>
2025-04-01 13:00:28 +0100

    IMTA-19871 Validate exit POE & exit BIP supplied for EU Ched-A when purpose = transit

--- notification-schema-java/src/main/java/uk/gov/defra/tracesx/notificationschema/validation/annotations/PortOfExitAndExitBipNotEmptyValidator.java
+++ notification-schema-java/src/main/java/uk/gov/defra/tracesx/notificationschema/validation/annotations/PortOfExitAndExitBipNotEmptyValidator.java
@@ -2,26 +2,38 @@ package uk.gov.defra.tracesx.notificationschema.validation.annotations;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import org.apache.commons.lang3.StringUtils;
 import uk.gov.defra.tracesx.notificationschema.representation.PartOne;
 import uk.gov.defra.tracesx.notificationschema.representation.enumeration.ForImportOrAdmissionEnum;
+import uk.gov.defra.tracesx.notificationschema.representation.enumeration.PurposeGroupEnum;
 
 public class PortOfExitAndExitBipNotEmptyValidator implements
     ConstraintValidator<PortOfExitAndExitBipNotEmpty, PartOne> {
 
   @Override
   public boolean isValid(PartOne partOne, ConstraintValidatorContext constraintValidatorContext) {
-    if (partOne == null) {
+    if (!isCandidateForValidation(partOne)) {
       return true;
     }
+    return !StringUtils.isBlank(partOne.getPortOfExit())
+        && !StringUtils.isBlank(partOne.getPurpose().getExitBIP());
+  }
 
-    if (partOne.getPurpose() == null
-        || !ForImportOrAdmissionEnum.TEMPORARY_ADMISSION_HORSES.equals(
-        partOne.getPurpose().getForImportOrAdmission())) {
-      return true;
-    }
+  private boolean isCandidateForValidation(PartOne partOne) {
+    return partOne != null
+        && partOne.getPurpose() != null
+        && (isTemporaryAdmissionHorses(partOne) || isTransit(partOne));
+  }
 
-    return partOne.getPortOfExit() != null && !partOne.getPortOfExit().isBlank()
-        && partOne.getPurpose().getExitBIP() != null
-        && !partOne.getPurpose().getExitBIP().isBlank();
+  private boolean isTemporaryAdmissionHorses(PartOne partOne) {
+    return ForImportOrAdmissionEnum.TEMPORARY_ADMISSION_HORSES.equals(
+        partOne.getPurpose().getForImportOrAdmission()
+    );
+  }
+
+  private boolean isTransit(PartOne partOne) {
+    return PurposeGroupEnum.TRANSIT_TO_3RD_COUNTRY.equals(
+        partOne.getPurpose().getPurposeGroup()
+    );
   }
 }
