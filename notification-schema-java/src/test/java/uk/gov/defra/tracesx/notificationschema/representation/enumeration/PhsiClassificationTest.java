5401a34d2c570642c19cac64bd39844bfe133bf9
ian.griffiths <ian.griffiths@defra.onmicrosoft.com>
2025-06-16 15:10:06 +0100

    IMTA-20056 - add missing schema definition

--- notification-schema-java/src/test/java/uk/gov/defra/tracesx/notificationschema/representation/enumeration/PhsiClassificationTest.java
+++ notification-schema-java/src/test/java/uk/gov/defra/tracesx/notificationschema/representation/enumeration/PhsiClassificationTest.java
@@ -2,37 +2,53 @@ package uk.gov.defra.tracesx.notificationschema.representation.enumeration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class PhsiClassificationTest {
-  private final static String MANDATORY_STRING = "Mandatory";
-  private final static String INVALID_STRING = "Invalid";
 
-  @Test
-  void givenAValidEnumValue_whenToStringCalled_shouldReturnStringValue() {
-    String enumResult = PhsiClassification.MANDATORY.toString();
+  private static final String MANDATORY_STRING = "Mandatory";
+  private static final String REDUCED_STRING = "Reduced";
+  private static final String CONTROLLED_STRING = "Controlled";
+  private static final String NOT_NOTIFIABLE_STRING = "NotNotifiable";
+  private static final String INVALID_STRING = "Invalid";
 
-    assertThat(enumResult).isEqualTo(MANDATORY_STRING);
+  private static Stream<Arguments> enumStringMatches() {
+    return Stream.of(
+        Arguments.of(PhsiClassification.MANDATORY, MANDATORY_STRING),
+        Arguments.of(PhsiClassification.REDUCED, REDUCED_STRING),
+        Arguments.of(PhsiClassification.CONTROLLED, CONTROLLED_STRING),
+        Arguments.of(PhsiClassification.NOT_NOTIFIABLE, NOT_NOTIFIABLE_STRING)
+    );
   }
 
-  @Test
-  void givenAValidEnumValue_whenGetValueCalled_shouldReturnValue() {
-    String enumResult = PhsiClassification.MANDATORY.getValue();
-
-    assertThat(enumResult).isEqualTo(MANDATORY_STRING);
+  @ParameterizedTest
+  @MethodSource("enumStringMatches")
+  void givenAValidEnumValue_whenToStringCalled_shouldReturnStringValue(PhsiClassification classification, String expected) {
+    String enumResult = classification.toString();
+    assertThat(enumResult).isEqualTo(expected);
   }
 
-  @Test
-  void givenAValueValid_whenFromValueCalled_shouldReturnEnumValue() {
-    PhsiClassification enumResult = PhsiClassification.fromValue(MANDATORY_STRING);
+  @ParameterizedTest
+  @MethodSource("enumStringMatches")
+  void givenAValidEnumValue_whenGetValueCalled_shouldReturnValue(PhsiClassification classification, String expected) {
+    String enumResult = classification.getValue();
+    assertThat(enumResult).isEqualTo(expected);
+  }
 
-    assertThat(enumResult).isEqualTo(PhsiClassification.MANDATORY);
+  @ParameterizedTest
+  @MethodSource("enumStringMatches")
+  void givenAValueValid_whenFromValueCalled_shouldReturnEnumValue(PhsiClassification classification, String expected) {
+    PhsiClassification enumResult = PhsiClassification.fromValue(expected);
+    assertThat(enumResult).isEqualTo(classification);
   }
 
   @Test
   void givenAnInvalidValue_whenFromValueCalled_shouldReturnNull() {
     PhsiDecision enumResult = PhsiDecision.fromValue(INVALID_STRING);
-
     assertThat(enumResult).isNull();
   }
 }
